17e6ec78128f2b73c6c861bf6b098eaa8035c0fc
Zach Feldman <zmf@frame.work>
2023-07-30 18:43:53 -0400

    Finish script with some help from ChatGPT

--- dokku-cf-tunnel-update.rb
+++ dokku-cf-tunnel-update.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/ruby
 require 'json'
+require 'net/http'
 
-CF_TUNNEL_CONFIG_DEFAULT = [{
-	"service": "http_status:404"
-}]
 DEFAULT_CF_TUNNEL_CONFIG_LOCATION = "/home/dokku/.cf-tunnel-config"
 
 puts "Reading /home/dokku/.cf-tunnel-config file, if it exists, or using default"
@@ -15,20 +13,57 @@ if File.exists?(DEFAULT_CF_TUNNEL_CONFIG_LOCATION)
 	)
 end
 
-cf_tunnel_config = cf_tunnel_config_file_contents_as_json || CF_TUNNEL_CONFIG_DEFAULT
+cf_tunnel_config = cf_tunnel_config_file_contents_as_json || []	
 
 puts "Updating ingress list with current apps"
 dokku_app_list = `dokku apps:list`.split("\n")[1..-1]
 
 dokku_app_list.each do |dokku_app|
-	hostname = `dokku domains:report #{dokku_app} --domains-app-vhosts`
-	service = `cat /home/dokku/#{dokku_app}/nginx.conf | grep "server .*5000" | awk '$1 == "server" {print $2}' | cut -f1 -d";"`
+	hostname = `dokku domains:report #{dokku_app} --domains-app-vhosts`.tr("\n","")
+	service = `cat /home/dokku/#{dokku_app}/nginx.conf | grep "server .*5000" | awk '$1 == "server" {print $2}' | cut -f1 -d";"`.tr("\n","")
 	if hostname && service
 		cf_tunnel_config.push({
-			hostname: hostname,
-			service: service
+			"hostname" => hostname,
+			"service" => service
 		})
 	end
 end
 
+cf_tunnel_config.push({
+	"service" => "http_status:404"
+})
+
 puts cf_tunnel_config
+
+cf_account_uuid = ENV['CF_ACCOUNT_UUID']
+cf_tunnel_uuid = ENV['CF_TUNNEL_UUID']
+cloudflare_api_url = "https://api.cloudflare.com/client/v4/accounts/#{cf_account_uuid}/cfd_tunnel/#{cf_tunnel_uuid}/configurations"
+
+
+cf_auth_email = ENV['CF_AUTH_EMAIL']
+cf_auth_key = ENV['CF_AUTH_KEY']
+headers = {
+  'Content-Type' => 'application/json',
+  'X-Auth-Email' => cf_auth_email,
+  'X-Auth-Key' => cf_auth_key
+}
+
+# Set up the request body
+data = {
+  "config": {
+    "ingress": cf_tunnel_config
+  }
+}
+
+uri = URI(cloudflare_api_url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+request = Net::HTTP::Put.new(uri.path, headers)
+request.body = data.to_json
+
+response = http.request(request)
+
+# Handle the response
+puts "Cloudflare API Response code: #{response.code}"
+puts "Cloudflare API Response body: #{response.body}"
